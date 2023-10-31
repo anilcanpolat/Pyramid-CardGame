5c9959c480c297a3d84b2e6d23026bd470717a06
anilcanpolat <anilcan.polat@tu-dortmund.de>
2023-10-31 01:09:59 +0100

    Documentation for view package is added.

--- src/main/kotlin/view/SopraApplication.kt
+++ src/main/kotlin/view/SopraApplication.kt
@@ -2,13 +2,29 @@ package view
 
 import tools.aqua.bgw.core.BoardGameApplication
 
+/**
+ * `SopraApplication` class represents the main application for the SoPra game.
+ * It extends the `BoardGameApplication` class from the `tools.aqua.bgw.core` package.
+ *
+ * Upon instantiation, it initializes and displays the `HelloScene` which showcases a greeting message.
+ *
+ * @property helloScene An instance of the `HelloScene` class.
+ *
+ * @constructor Creates an instance of the `SopraApplication` class with the title "SoPra Game".
+ */
 class SopraApplication : BoardGameApplication("SoPra Game") {
 
-   private val helloScene = HelloScene()
+    /**
+     * Creates and holds an instance of the `HelloScene`.
+     */
+    private val helloScene = HelloScene()
 
-   init {
+    /**
+     * Initialization block.
+     * Sets and displays the `helloScene` as the main scene of the application.
+     */
+    init {
         this.showGameScene(helloScene)
     }
 
 }
-
